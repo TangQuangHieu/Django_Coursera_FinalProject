c550d2169761329049c8ffebb4215208c06d22b7
Your Name <you@example.com>
2023-04-18 08:08:08 -0400

    Modified

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
-
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 5 
+class ChoiceInline(admin.StackedInline):
+    model = Choice 
+    extra = 5
+class QuestionAdmin(admin.ModelAdmin):
+    inlines=[ChoiceInline]
+    list_display=('question_text','grade')
+    list_filter=['lesson','grade']
+    search_fields=['lesson','question_text']
 
 class LessonInline(admin.StackedInline):
     model = Lesson
@@ -23,7 +33,8 @@ class LessonAdmin(admin.ModelAdmin):
 
 
 # <HINT> Register Question and Choice models here
-
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice)
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -110,18 +110,24 @@ def enroll(request, course_id):
          # Collect the selected choices from exam form
          # Add each selected choice object to the submission object
          # Redirect to show_exam_result with the submission id
-#def submit(request, course_id):
+def submit(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    user = request.user
+    enrollment = Enrollment.objects.filter(user=user, course=course)
+    submission = Submission.objects.create(enrollment=enrollment)
+    submitted_answers = extract_answers(request)
+    submission
 
 
 # <HINT> A example method to collect the selected choices from the exam form from the request object
-#def extract_answers(request):
-#    submitted_anwsers = []
-#    for key in request.POST:
-#        if key.startswith('choice'):
-#            value = request.POST[key]
-#            choice_id = int(value)
-#            submitted_anwsers.append(choice_id)
-#    return submitted_anwsers
+def extract_answers(request):
+   submitted_anwsers = []
+   for key in request.POST:
+       if key.startswith('choice'):
+           value = request.POST[key]
+           choice_id = int(value)
+           submitted_anwsers.append(choice_id)
+   return submitted_anwsers
 
 
 # <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
